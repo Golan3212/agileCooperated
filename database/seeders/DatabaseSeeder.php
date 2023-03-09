23916adce30efa81e110a55c4f980684c5ea88dd
Golan3212 <93038099+Golan3212@users.noreply.github.com>
2023-03-09 13:12:41 +0600

    Category/ingredient/recipes Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            IngredientsSeeder::class,
+            CategorySeeder::class,
+            RecipesSeeder::class,
+            RecipesHasIngredientsSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/RecipesHasIngredientsSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\MassUnit;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class  RecipesHasIngredientsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run():void
+    {
+        DB::table('recipes_has_ingredients')->insert($this->getData());
+    }
+
+
+    public function getData():array
+    {
+        $recipesQuantity = DB::table('recipes')->count('id');
+        $ingredients = DB::table('ingredients')->count('id');
+
+        $data = [];
+        for ($i = 1; $i <= $recipesQuantity; $i++){
+                $data[] =
+                    [
+                        'recipes_id' => $i,
+                        'recipes_category_id'=> DB::table('recipes')->where('id', '=', $i)
+                            ->value('category_id'),
+                        'ingredients_id'=> random_int(1, $ingredients)
+                    ];
+            }
+        return $data;
+    }
+}
