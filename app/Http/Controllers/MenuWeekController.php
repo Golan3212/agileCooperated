12e5be7ab11ed07d41c03a08fb948c474c17e5e8
Veronika Nikitina <tomova_veronika@mail.ru>
2023-03-29 12:49:40 +0300

    Слайдер, Рецепт, Контсруктор

--- app/Http/Controllers/MenuWeekController.php
+++ app/Http/Controllers/MenuWeekController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\RecipeRequest;
 use App\QueryBuilders\MenuQueryBuilder;
 use App\QueryBuilders\MenuWeekQueryBuilder;
 use App\QueryBuilders\RecipesQueryBuilder;
@@ -50,10 +49,10 @@ class MenuWeekController extends Controller
                 'totalCarbohydrates' => $item->total_carboh_ydrates,
                 'menu_recipes'=> [
                     $item->breakfest->toArray(),
-                    $item->dinner->toArray(),
-                    $item->lunch->toArray(),
                     $item->firstSnack->toArray(),
+                    $item->dinner->toArray(),
                     $item->secondSnack->toArray(),
+                    $item->lunch->toArray(),
                 ]
             ];
         };
@@ -66,33 +65,4 @@ class MenuWeekController extends Controller
         ]);
     }
 
-    public function show(
-        RecipesQueryBuilder $recipesQueryBuilder,
-        int $category_id
-    )
-    {
-
-        $recipeAdvicesList = $recipesQueryBuilder->getRecipeByCategoryId($category_id)->random(8);
-
-        $recipeOneAdvice =[];
-        foreach ($recipeAdvicesList as $key => $value) {
-            $recipeOneAdvice[] = [
-                'id' => $value->id,
-                'category_id' => $value->category_id,
-                'title' => $value->title,
-                'calorie' => $value->calorie,
-                'proteins' => $value->proteins,
-                'fats' => $value->fats,
-                'carbohydrates' => $value->carbohydrates,
-                'portion' => $value->portion,
-                'cooking_time' => $value->cooking_time,
-                'category_title' => $value->category->title,
-            ];
-        }
-
-
-        return Inertia::render('CategoryBuilder', [
-            'recipeOneAdvice' => $recipeOneAdvice,
-        ]);
-    }
 }
